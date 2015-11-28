698a65eb6869214b09851acbdd0c189beafefeca
Charles Watson <nchlswtsn@gmail.com>
2015-11-28 10:46:33 -0800

    complete basic music functionality

--- public/javascripts/controllers/navigation.js
+++ public/javascripts/controllers/navigation.js
@@ -4,16 +4,56 @@ var app = angular.module('fantasyFitness')
   $scope.currentUser = auth.currentUser();
   $scope.logOut = auth.logOut;
   var songs = ["violin", "stronger", "boom"]
-  var currentSong = new Audio("music/" + songs[2] + ".mp3")
-    $scope.playStatus = true;
+  var songIndex = 1;
+  var currentSong = new Audio("music/" + songs[songIndex] + ".mp3")
+  $scope.playStatus = true;
   $scope.playPause = function() {
-    console.log($scope.playStatus);
-    console.log("Click!");
     if (!$scope.playStatus) currentSong.pause();
     else currentSong.play();
     $scope.playStatus = !$scope.playStatus;
   }
-  $scope.slideOut = function() {
-    
+  $scope.stopSong = function() {
+    currentSong.pause();
+    currentSong.currentTime = 0;
+    $scope.playStatus = true;
+  }
+  $scope.previousSong = function() {
+    currentSong.pause()
+    console.log(songIndex);
+    if (songIndex === 0) {
+      songIndex = songs.length - 1;
+    } else {
+      songIndex -= 1;
+    }
+    currentSong = new Audio("music/" + songs[songIndex] + ".mp3");
+    currentSong.play();
+    $scope.playStatus = false;
+  }
+  $scope.nextSong = function() {
+    console.log(songIndex);
+    currentSong.pause()
+    if (songIndex === songs.length -1) {
+      songIndex = 0;
+    } else {
+      songIndex += 1;
+    }
+    currentSong = new Audio("music/" + songs[songIndex] + ".mp3");
+    currentSong.play();
+    $scope.playStatus = false;
+  }
+  $scope.fastForward = function() {
+    currentSong.pause();
+    currentSong.currentTime += 10;
+    currentSong.play();
+  }
+  $scope.rewind = function() {
+    currentSong.pause();
+    currentSong.currentTime -= 10;
+    currentSong.play();
+  }
+
+  // SPACE PAUSE
+  $scope.keyControl = function() {
+    console.log(event.charCode);
   }
 }]);
