db5dba1b85f9d02be892e3a974c63787563f404b
Charles Watson <nchlswtsn@gmail.com>
2015-11-28 16:55:08 -0800

    add auto next for end of song

--- public/javascripts/controllers/navigation.js
+++ public/javascripts/controllers/navigation.js
@@ -2,6 +2,7 @@ var app = angular.module('fantasyFitness')
 .controller('NavCtrl', ['$scope', 'auth','$location', function($scope, auth, $location) {
   $scope.isLoggedIn = auth.isLoggedIn;
   $scope.currentUser = auth.currentUser();
+
   $scope.logOut = function(){
     auth.logOut;
     localStorage.clear();
@@ -10,7 +11,7 @@ var app = angular.module('fantasyFitness')
   var songIndex = Math.ceil(Math.random()*6);
   var songs = ["violin.mp3", "stronger.mp3", "boom.mp3", "blank.m4a", "energy.mp3", "dynamite.m4a", "charlesqueen.m4a"]
   var currentSong = new Audio("music/" + songs[songIndex])
-    $scope.playStatus = true;
+  $scope.playStatus = true;
   $scope.playPause = function() {
     if (!$scope.playStatus) currentSong.pause();
     else currentSong.play();
@@ -55,9 +56,7 @@ var app = angular.module('fantasyFitness')
     currentSong.currentTime -= 10;
     currentSong.play();
   }
-
-  // SPACE PAUSE
-  $scope.keyControl = function() {
-    console.log(event.charCode);
-  }
+  $scope.playThrough = function() {
+    setTimeout($scope.nextSong, currentSong.duration * 1000);
+  };
 }]);
